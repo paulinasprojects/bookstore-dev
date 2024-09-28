1769094868e5a527b85de559c0a0b67c64698892
paulinasprojects <virtualpaula93@gmail.com>
2024-09-28 14:19:41 +0300

    added confetti on the success page

--- src/pages/checkout-success.tsx
+++ src/pages/checkout-success.tsx
@@ -1,9 +1,17 @@
 import successImage from '/success.png';
 import "@/styles/checkout-success.scss";
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 const CheckoutSuccess = () => {
+  const { height, width } = useWindowSize();
+
   return (
-    
+    <>
+    <Confetti
+      width={width}
+      height={height}
+    />
     <div className="checkout-success-container">
       <img src={successImage} alt="" />
       <div className='checkout-title-container'>
@@ -15,6 +23,8 @@ const CheckoutSuccess = () => {
          <button className='checkout-success-button'>Transactions</button>
       </div>
     </div>
+    
+    </>
   )
 }
 
