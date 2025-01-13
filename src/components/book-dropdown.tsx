fac1a1f54308d40e4c4af664184fb412ea7be475
paulinasprojects <virtualpaula93@gmail.com>
2025-01-13 15:43:49 +0200

    added actual book dropdown

--- src/components/book-dropdown.tsx
+++ src/components/book-dropdown.tsx
@@ -1,33 +1,26 @@
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import {Button,ListBox, ListBoxItem, Popover, Select, SelectValue, Label} from 'react-aria-components';
 import { BiChevronDown } from 'react-icons/bi';
 
 import "@/styles/book-dropdown.scss";
 
 const BookDropdown = () => {
   return (
-    <Menu>
-    <MenuButton className="menu-button">
-      Category
-      <BiChevronDown className='book-dropdown-chevron'/>
-    </MenuButton>
-    <MenuItems anchor="bottom" className="book-dropdown-menu-items">
-      <MenuItem>
-        <a className="book-dropdown-menu-item" href="#">
-          Price asc
-        </a>
-      </MenuItem>
-      <MenuItem>
-        <a className="book-dropdown-menu-item" href="#" >
-          Price desc
-        </a>
-      </MenuItem>
-      <MenuItem>
-        <a className="book-dropdown-menu-item" href="#" >
-          Reviews
-        </a>
-      </MenuItem>
-    </MenuItems>
-  </Menu>
+    <Select placeholder='Category' aria-label='select-group'>
+      <Label style={{display: "none"}} aria-label='radio-group'>Label</Label>
+      <Button>
+        <SelectValue/>
+        <span aria-hidden="true">
+          <BiChevronDown/>
+        </span>
+      </Button>
+      <Popover>
+        <ListBox>
+          <ListBoxItem>Price asc</ListBoxItem>
+          <ListBoxItem>Price desc</ListBoxItem>
+          <ListBoxItem>Reviews</ListBoxItem>
+        </ListBox>
+      </Popover>
+    </Select>
   )
 }
 
